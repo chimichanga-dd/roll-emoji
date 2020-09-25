0b9bbd12c84769deaefe08cf7d82111e4eaf3eda
David Dixon <david.dixon@centene.com>
2020-09-25 14:48:48 -0700

    change router

--- router/routes.js
+++ router/routes.js
@@ -11,19 +11,15 @@ router.get('/api/tester/', cors(), async(req,res,next) => {
   }
 })
 
-// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-//   router.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/frontEnd/public/index.html'))
-//   })
-// } else {
-//   router.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/frontEnd/build/index.html'))
-//   })
-// }
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontEnd/public/index.html'))
+  })
+} else {
+  router.get('*', (req, res) => {
+    res.sendFile(path.join('app/frontEnd/build/index.html'))
+  })
+}
 
-
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontEnd/build/index.html'))
-})
 
 module.exports = router;
