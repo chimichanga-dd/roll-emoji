6feb900977f2bb46090104cf6543c9ff26b4e001
David Dixon <david.dixon@centene.com>
2020-09-25 14:03:55 -0700

    change index and routes for sockets

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const express = require('express')
-const cors = require('cors')
+const http = require('http')
 const path = require('path')
-const  app = express()
+const socketIo = require('socket.io')
+const app = express()
+const routes = require('./router/routes')
 
-app.use(express.static(path.join(__dirname, 'frontEnd/build')))
+app.use(routes);
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('/api/tester/', cors(), async(req,res,next) => {
-  try {
-    res.json({testMessage:'You better be able to see this binch'})
-  } catch (err) {
-    next(err)
-  }
-})
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected")
+  socket.emit('connected','Someone just connected')
+
+  socket.on("disconnect", (msg) => {
+    io.emit('message Received', msg)
+  });
+
+  socket.on('message Sent', (msg) => {
+    io.emit('message Received', msg)
+  });
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontEnd/build/index.html'))
-})
+});
 
 const PORT = process.env.PORT || 5000
 
-app.listen( PORT, () => {
+server.listen( PORT, () => {
   console.log(`Mixing it up on port ${PORT}`)
 })
