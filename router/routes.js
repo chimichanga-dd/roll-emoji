09ef41c5176cb66aa703aaa21aa0368482f20a5c
David Dixon <david.dixon@centene.com>
2020-09-25 15:10:17 -0700

    fix routes

--- router/routes.js
+++ router/routes.js
@@ -13,11 +13,11 @@ router.get('/api/tester/', cors(), async(req,res,next) => {
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontEnd/public/index.html'))
+    res.sendFile(path.join(__dirname, '../frontEnd/public/index.html'))
   })
 } else {
   router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontEnd/build/index.html'))
+    res.sendFile(path.join(__dirname, '../frontEnd/build/index.html'))
   })
 }
 
