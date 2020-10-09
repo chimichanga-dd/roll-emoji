d0fdfaa6b087ed808cb4a2a72448221149dd1545
David Dixon <david.dixon@centene.com>
2020-10-09 12:16:47 -0700

    move emoji maker

--- frontEnd/src/components/emoji/emojiButton.js
+++ frontEnd/src/components/emoji/emojiButton.js
@@ -1,42 +1,8 @@
 import React from "react"
-import $ from "jquery"
 import {Button} from "semantic-ui-react"
 
 
-function create(svg, i) {
-  var width = 50;
-
-  $("<div class=emoji-" + svg.alt + "-" + i +"><img src='" + svg.src + "' alt=" + svg.alt + "/></div>").css({
-    "width" : width+"px",
-    "top" : -Math.random()*20+"%",
-    "left" : Math.random()*90+"%",
-    "opacity" : Math.random()+0.5,
-    "transform" : Math.random() > .5 ? "rotate("+Math.random()*90+"deg)" : "rotate(-"+Math.random()*90+"deg)"
-  }).appendTo('.main-body');
-
-  drop(svg, i);
-}
-
-function drop(svg, x) {
-  $('.emoji-' + svg.alt + "-" +x).animate({
-    top: "90%",
-    left: +Math.random()*90+"%"
-  }, Math.random()*3000 + 3000, function() {
-    remove(svg,x);
-  });
-}
-
-function remove(svg, x) {
-  $('.emoji-' + svg.alt + "-" + x).remove()
-}
-
-const makeConfetti = (svg) => {
-  for (var i = 0; i < 50; i++) {
-    create(svg,i);
-  }
-}
-
-const EmojiButton = ({svg}) => {
+const EmojiButton = ({makeConfetti, svg}) => {
   return (
     <Button
       className="ui secondary button"
--- frontEnd/src/components/emoji/emojiContainer.js
+++ frontEnd/src/components/emoji/emojiContainer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import $ from "jquery"
 import './emojiContainer.scss'
 import okay from '../../svg/okay.svg';
 import thumbsUp from "../../svg/thumbsUp.svg"
@@ -7,6 +8,40 @@ import rose  from "../../svg/rose.svg"
 import laughing from "../../svg/laughing.svg"
 import poop from "../../svg/poop.svg"
 import EmojiButton from "./emojiButton";
+
+
+function create(svg, i) {
+  var width = 50;
+
+  $("<div class=emoji-" + svg.alt + "-" + i +"><img src='" + svg.src + "' alt=" + svg.alt + "/></div>").css({
+    "width" : width+"px",
+    "top" : -Math.random()*20+"%",
+    "left" : Math.random()*90+"%",
+    "opacity" : Math.random()+0.5,
+    "transform" : Math.random() > .5 ? "rotate("+Math.random()*90+"deg)" : "rotate(-"+Math.random()*90+"deg)"
+  }).appendTo('.main-body');
+
+  drop(svg, i);
+}
+
+function drop(svg, x) {
+  $('.emoji-' + svg.alt + "-" +x).animate({
+    top: "90%",
+    left: +Math.random()*90+"%"
+  }, Math.random()*3000 + 3000, function() {
+    remove(svg,x);
+  });
+}
+
+function remove(svg, x) {
+  $('.emoji-' + svg.alt + "-" + x).remove()
+}
+
+const makeConfetti = (svg) => {
+  for (var i = 0; i < 50; i++) {
+    create(svg,i);
+  }
+}
 
 
 const EmojiContainer = () => {
@@ -22,7 +57,7 @@ const EmojiContainer = () => {
 
   return (
     <div className="e-container">
-      {emojis.map(svg => <EmojiButton svg={svg} key={svg.alt}/>)}
+      {emojis.map(svg => <EmojiButton makeConfetti={makeConfetti} svg={svg} key={svg.alt}/>)}
     </div>
   )
 
