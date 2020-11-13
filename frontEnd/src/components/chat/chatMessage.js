25d4452396dca7e06cd61d91c31179a27b7d84b3
David Dixon <david.dixon@centene.com>
2020-11-13 12:18:41 -0800

    limit chat size and add message color

--- frontEnd/src/components/chat/chatMessage.js
+++ frontEnd/src/components/chat/chatMessage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./chatMessage.scss"
 
 
 const ChatMessage = ({messageObject}) => {
@@ -6,15 +7,25 @@ const ChatMessage = ({messageObject}) => {
   const formatMessage = (messageObject) => {
     switch(messageObject.type){
       case 'message':
-        return <div>{messageObject.person}: {messageObject.message}</div>
+        return <div
+          className={"standard-message"}
+          >{messageObject.person}: {messageObject.message}</div>
       case 'reaction':
-        return <div>{messageObject.person} {messageObject.message}</div>
+        return <div
+          className={"reaction-message"}
+        >{messageObject.person} {messageObject.message}</div>
       case 'rollMessage':
-        return <div>{messageObject.person} rolled a {messageObject.num}</div>
+        return <div
+          className={"roll-message"}
+        >{messageObject.person} rolled a {messageObject.num}</div>
       case 'disconnected':
-        return <div>{messageObject.person} has left</div>
+        return <div
+          className={"disconnect-message"}
+        >{messageObject.person} has left</div>
       case 'connected':
-        return <div>{messageObject.person} has joined</div>
+        return <div
+          className={"connect-message"}
+        >{messageObject.person} has joined</div>
       default:
         return <div>IDK WHAT WOULD GET THIS</div>
     }
