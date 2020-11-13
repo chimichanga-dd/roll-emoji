9c84ba93fc6eacd77b753f351486fe297c7183a7
David Dixon <david.dixon@centene.com>
2020-11-13 10:59:59 -0800

    button colors

--- frontEnd/src/components/emoji/emojiButton.js
+++ frontEnd/src/components/emoji/emojiButton.js
@@ -1,11 +1,11 @@
 import React from "react"
 import {Button} from "semantic-ui-react"
-
+import "./emojiButton.scss"
 
 const EmojiButton = ({emitEmoji, svg}) => {
   return (
     <Button
-      className="ui secondary button"
+      className="emoji-button"
       onClick={() => emitEmoji(svg)}>
       <img className={"button-emoji"} src={svg.src} alt={svg.alt}/>
     </Button>
