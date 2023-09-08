f506ba1ffc6ec6064ee3d24e443b25bba6935523
David Dixon <chimichanga@g.ucla.edu>
2023-09-08 22:47:38 +0700

    handle empty persons

--- frontEnd/src/components/chat/chat.js
+++ frontEnd/src/components/chat/chat.js
@@ -1,58 +1,65 @@
-import React, { useEffect, useRef, useState } from "react"
-import ChatInput from "./chatInput"
-import ChatMessage from "./chatMessage"
-import socket from '../socket/socketIO'
-import { useAuthState } from "../auth/authContext"
-import airhorn from "../../audio/airhorn.wav"
+import React, { useEffect, useRef, useState } from "react";
+import ChatInput from "./chatInput";
+import ChatMessage from "./chatMessage";
+import socket from "../socket/socketIO";
+import { useAuthState } from "../auth/authContext";
+import airhorn from "../../audio/airhorn.wav";
 
 const Chat = () => {
+  const {
+    state: { user }
+  } = useAuthState();
+  const [chatMessages, setChatMessages] = useState([]);
+  const bottomOfBox = useRef(null);
+  const audioRef = useRef(null);
 
-  const {state: {user}} = useAuthState()
-  const [chatMessages, setChatMessages] = useState([])
-  const bottomOfBox = useRef(null)
-  const audioRef = useRef(null)
+  useEffect(() => {
+    socket.emit("connected", user);
 
+    socket.on("message Received", (message) => {
+      setChatMessages((chatMessages) => [...chatMessages, message]);
 
-  useEffect(()=> {
-    socket.emit('connected', user)
-
-    socket.on('message Received', (message) => {
-      setChatMessages( (chatMessages) => [...chatMessages, message])
-
-      if(audioRef.current && message.type === "reaction" && message.message.includes("air horn")){
-        audioRef.current.volume = 0.2
-        if(!audioRef.current.paused){
-          audioRef.current.currentTime = 0
+      if (
+        audioRef.current &&
+        message.type === "reaction" &&
+        message.message.includes("air horn")
+      ) {
+        audioRef.current.volume = 0.2;
+        if (!audioRef.current.paused) {
+          audioRef.current.currentTime = 0;
         } else {
-          audioRef.current.play()
+          audioRef.current.play();
         }
       }
-    })
+    });
 
-    return (
-      () => {
-        socket.off('message Received')
-      }
-    )
+    return () => {
+      socket.off("message Received");
+    };
+  }, []);
 
-  }, [])
-
-  useEffect( () => {
-    bottomOfBox.current.scrollIntoView({ behavior: "smooth" })
-  },[chatMessages])
-
+  useEffect(() => {
+    bottomOfBox.current.scrollIntoView({ behavior: "smooth" });
+  }, [chatMessages]);
 
   return (
     <div className="ui segment chat-box">
       <div className="chat-messages">
-        {chatMessages.map((messageObject, index) => <ChatMessage messageObject={messageObject} key={index}/>)}
+        {chatMessages.map((messageObject, index) => (
+          <ChatMessage
+            messageObject={messageObject}
+            key={index}
+          />
+        ))}
         <div ref={bottomOfBox}></div>
       </div>
-      <ChatInput user={user}/>
-      <audio ref={audioRef} src={airhorn}/>
+      <ChatInput user={user} />
+      <audio
+        ref={audioRef}
+        src={airhorn}
+      />
     </div>
-  )
+  );
+};
 
-}
-
-export default Chat
+export default Chat;
--- frontEnd/src/components/chat/chatMessage.js
+++ frontEnd/src/components/chat/chatMessage.js
@@ -1,39 +1,53 @@
-import React from "react"
-import "./chatMessage.scss"
+import React from "react";
+import "./chatMessage.scss";
 
-
-const ChatMessage = ({messageObject}) => {
-
-  const formatMessage = (messageObject) => {
-    switch(messageObject.type){
-      case 'message':
-        return <div
-          className={"standard-message"}
-          >{messageObject.person}: {messageObject.message}</div>
-      case 'reaction':
-        return <div
-          className={"reaction-message"}
-        >{messageObject.person} {messageObject.message}</div>
-      case 'rollMessage':
-        return <div
-          className={"roll-message"}
-        >{messageObject.person} rolled a {messageObject.num}</div>
-      case 'disconnected':
-        return <div
-          className={"disconnect-message"}
-        >{messageObject.person} has left</div>
-      case 'connected':
-        return <div
-          className={"connect-message"}
-        >{messageObject.person} has joined</div>
-      default:
-        return <div>IDK WHAT WOULD GET THIS</div>
-    }
+const ChatMessage = ({ messageObject }) => {
+  if (
+    !messageObject ||
+    messageObject.person === null ||
+    messageObject.person === undefined
+  ) {
+    return null;
   }
 
-  return (
-    formatMessage(messageObject)
-  )
-}
+  const formatMessage = (messageObject) => {
+    switch (messageObject.type) {
+      case "message":
+        return (
+          <div className={"standard-message"}>
+            {messageObject.person}: {messageObject.message}
+          </div>
+        );
+      case "reaction":
+        return (
+          <div className={"reaction-message"}>
+            {messageObject.person} {messageObject.message}
+          </div>
+        );
+      case "rollMessage":
+        return (
+          <div className={"roll-message"}>
+            {messageObject.person} rolled a {messageObject.num}
+          </div>
+        );
+      case "disconnected":
+        return (
+          <div className={"disconnect-message"}>
+            {messageObject.person} has left
+          </div>
+        );
+      case "connected":
+        return (
+          <div className={"connect-message"}>
+            {messageObject.person} has joined
+          </div>
+        );
+      default:
+        return <div>IDK WHAT WOULD GET THIS</div>;
+    }
+  };
 
-export default ChatMessage
+  return formatMessage(messageObject);
+};
+
+export default ChatMessage;
